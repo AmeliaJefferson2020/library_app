bd46d02ed215891ccc6cb1ab4f9695b5541ada34
AmeliaJefferson2020 <ameliathompson.ace@gmail.com>
2026-01-30 17:51:47 -0500

    Finished App

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,22 +1,23 @@
-import UndrawBooks from '../assets/Undraw_Books.svg'
-import { Link } from "react-router-dom"
+import UndrawBooks from "../assets/Undraw_Books.svg";
 
 const Landing = () => {
-    return (
-        <section id="landing">
-            <header>
-                <div className="header__container">
-                    <div className="header__description">
-                        <h1>America's Most Awarded Online Library Platform</h1>
-                        <h2>Find Your Dream Book With <span className="purple">Library</span></h2>
-                    </div>
-                    <figure className="header__img--wrapper">
-                        <img src={UndrawBooks} alt="" />
-                    </figure>
-                </div>
-            </header>
-        </section>
-    )
-}
+  return (
+    <section id="landing">
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>America's Most Awarded Online Library Platform</h1>
+            <h2>
+              Find Your Dream Book With <span className="purple">Library</span>
+            </h2>
+          </div>
+          <figure className="header__img--wrapper">
+            <img src={UndrawBooks} alt="" />
+          </figure>
+        </div>
+      </header>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;
